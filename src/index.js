8e970621c8aed75efda87edf528ea364891e1335
hoooooojjjj <rhj080471@gmail.com>
2023-11-09 19:48:47 +0900

    vanilla-redux로 todolist 프로그램 만들기

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { createStore } from "redux"; // 상태를 저장하는 저장소
 
+// *** Counter 프로그램 ***
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
@@ -36,3 +38,60 @@ add.addEventListener("click", () => {
 minus.addEventListener("click", () => {
   countStore.dispatch({ type: MINUS });
 });
+
+// *** ToDoList 프로그램 ***
+
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [{ text: action.text, id: Date.now() }, ...state];
+    case DELETE_TODO:
+      return state.filter((todo) => todo.id !== parseInt(action.id));
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+const addToDo = (text) => {
+  store.dispatch({ type: ADD_TODO, text });
+};
+
+const deleteTodo = (e) => {
+  const id = e.target.parentNode.id;
+  store.dispatch({ type: DELETE_TODO, id });
+};
+
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach((todo) => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", deleteTodo);
+    li.id = todo.id;
+    li.innerText = todo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+};
+
+store.subscribe(paintToDos);
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  addToDo(toDo);
+};
+
+form.addEventListener("submit", onSubmit);
